6bacf511125f67807705976b6363acacfeafef88
MinhAn <voongocminhan20072002@gmail.com>
2023-09-11 00:07:00 +0700

    Run in local

--- src/main/java/vn/edu/fit/iuh/lab1/controllers/CotrollerServlet.java
+++ src/main/java/vn/edu/fit/iuh/lab1/controllers/CotrollerServlet.java
@@ -1,4 +1,32 @@
 package vn.edu.fit.iuh.lab1.controllers;
+import java.io.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+//
+//import jakarta.servlet.annotation.WebServlet;
+//
+//
+//
+//public class CotrollerServlet {
+//}
 
-public class CotrollerServlet {
-}
+@WebServlet(name = "CotrollerServlet", value = "/servlet")
+public class CotrollerServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello World!";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+    }
+
+    public void destroy() {
+    }}
